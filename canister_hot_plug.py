cbad133b8eadb8086ebc2e953bfc6d210b8c93e3
wgz <bajunma123@163.com>
2016-08-14 11:41:16 +0800

    add write to file function

--- canister_hot_plug.py
+++ canister_hot_plug.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 
 import time
-from subprocess import *
+from subprocess import Popen, PIPE
 
 
 ###############################################################################
@@ -15,60 +15,58 @@ from subprocess import *
 # Canister Hot Plug 
 # Check SES Page 02h  
 
+# get the result
 def get_information(sg_ses='sg_ses', page=None, index=None):
     for device_number in range(14):
         expre = '[root@localhost wgz]# sg_ses {} {} /dev/sg{}\n'.format(page, index, device_number)
         test_result.append(expre)
         proc = Popen([sg_ses, page, index, '/dev/sg{}'.format(device_number)], stdout=PIPE, stderr=PIPE)
         output, errors = proc.communicate() 
-        test_result.append((output+errors).decode('utf-8'))
+        test_result.append((output + errors).decode('utf-8'))
     return test_result.append(('-' * 80 + '\n'))
 
-print('SES-13-2--SES001001.txt')
+# write and select the related result info file and output it into the screen
+def write_info_file(test_id):
+    with open(test_id, 'w') as f:
+        for num, content in result.items():
+            if 'enclosure' in content:
+                f.write(result[num-1])
+                f.write(content)
+                f.write(('-'*80+'\n'))
+    print(test_id)
+    file_content = open(test_id).read()
+    print(file_content)
+
+###############################################################
 
 test_result =  []
-
 get_information(page='--page=0x02', index='--index=esc,0')
-
 print('Remove Canister A')
-time.sleep(5)
-
+time.sleep(2)
 get_information(page='--page=0x02', index='--index=esc,0')
-
 print('Insert Canister A')
-time.sleep(5)
+time.sleep(2)
 
-with open('SES-13-2--SES001001.txt', 'w') as f:
-    for content in test_result:
-        f.write(content)
+result = dict(enumerate(test_result))
+# print(result)
+write_info_file('SES-13-2--SES001001.txt')
 
-# judge and scan the results
-
-file_content = open('SES-13-2--SES001001.txt').read()
-print(file_content)
 
 
 ###############################################################
 
 # Canister Hot Plug 
-# Check SES Page 02h  
+# Check SES Page 07h  
 
-print('SES-13-2--SES001002.txt')
 
 test_result =  []
-
 get_information(page='--page=0x07', index='--index=esc,1')
-
 print('Remove Canister B')
 time.sleep(5)
-
 get_information(page='--page=0x07', index='--index=esc,1')
 
-with open('SES-13-2--SES001002.txt', 'w') as f:
-    for content in test_result:
-        f.write(content)
+result = dict(enumerate(test_result))
+# print(result)
+write_info_file('SES-13-2--SES001002.txt')
 
-# judge and scan the results
 
-file_content = open('SES-13-2--SES001002.txt').read()
-print(file_content)
